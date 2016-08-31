a41c8e484a28a53f4f5f2e0b48fc4d0e38d48d20
zhangaifei <zhangaifei@jd.com>
2016-08-31 15:29:50 +0800

    修复border问题

--- gulpfile.js
+++ gulpfile.js
@@ -8,36 +8,36 @@ const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
 gulp.task('styles', () => {
-  return gulp.src('src/wallet-ui.scss')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions']}))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(reload({stream: true}));
+    return gulp.src('src/wallet-ui.scss')
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass.sync({
+            outputStyle: 'expanded',
+            precision: 10,
+            includePaths: ['.']
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({ browsers: ['> 1%', 'last 2 versions'] }))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(reload({ stream: true }));
 });
 
 //压缩css文件
-gulp.task('cssmin', ()=>{
-  return gulp.src('src/wallet-ui.scss')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions']}))
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/styles'))
-    .pipe($.cssnano())
-    .pipe($.rename('wallet-ui.min.css'))
-    .pipe(gulp.dest('dist/styles'))
+gulp.task('cssmin', () => {
+    return gulp.src('src/wallet-ui.scss')
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass.sync({
+            outputStyle: 'expanded',
+            precision: 10,
+            includePaths: ['.']
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({ browsers: ['> 1%', 'last 2 versions'] }))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe($.cssnano())
+        .pipe($.rename('wallet-ui.min.css'))
+        .pipe(gulp.dest('dist/styles'))
 })
 gulp.task('clean', del.bind(null, ['dist']));
 
@@ -45,28 +45,32 @@ gulp.task('clean', del.bind(null, ['dist']));
 // 1. css和js在dist中 修改了=>stream
 // 2. images 和html在src中 修改=>reload
 gulp.task('server', ['styles'], () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['src', 'dist']
-    },
-    startPath:'/examples/index.html'
-  });
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['src', 'dist']
+        },
+        startPath: '/examples/index.html'
+    });
 
-  gulp.watch([
-    'src/**/*.html',
-    'src/images/**/*'
-  ]).on('change', reload);
+    gulp.watch([
+        'src/**/*.html',
+        'src/images/**/*'
+    ]).on('change', reload);
 
-  gulp.watch('src/**/*.scss', ['styles']);
-  // gulp.watch('src/scripts/**/*.js', ['scripts']);
+    gulp.watch('src/**/*.scss', ['styles']);
+    // gulp.watch('src/scripts/**/*.js', ['scripts']);
 });
 //复制字体文件到dist目录下
-gulp.task('cp-font', ()=>{
-  return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/styles/fonts'))
-})
-// 构建production包 显示gzip压缩之后的文件大小
-gulp.task('build',['cp-font', 'cssmin'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true, showFiles: true}));
+gulp.task('cp-font', () => {
+        return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/styles/fonts'))
+    })
+    // 构建production包 显示gzip压缩之后的文件大小
+gulp.task('build', ['cp-font', 'cssmin'], () => {
+    return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true, showFiles: true }));
 });
+// default task
+gulp.task('default', () => {
+    gulp.start('server');
+})
--- src/scripts/rem.js
+++ src/scripts/rem.js
@@ -13,56 +13,54 @@
        # return Boolean;
        # ps:请尽量第一时间运行此js计算字体
        */
-      !function (win, option) {
-          var count = 0,
-              designWidth = option.designWidth,
-              designHeight = option.designHeight || 0,
-              designFontSize = option.designFontSize || 20,
-              callback = option.callback || null,
-              root = document.documentElement,
-              body = document.body,
-              rootWidth, newSize, t, self;
+! function(win, option) {
+    var count = 0,
+        designWidth = option.designWidth,
+        designHeight = option.designHeight || 0,
+        designFontSize = option.designFontSize || 20,
+        callback = option.callback || null,
+        root = document.documentElement,
+        body = document.body,
+        rootWidth, newSize, t, self;
 
-          root.style.width = '100%';
-          //返回root元素字体计算结果
-          function _getNewFontSize() {
-              //var scale = designHeight !== 0 ? Math.min(win.innerWidth / designWidth, win.innerHeight / designHeight) : win.innerWidth / designWidth;
-              var scale = (window.innerWidth) / designWidth;
-              return parseInt(scale * 10000 * designFontSize) / 10000;
-          }
+    root.style.width = '100%';
+    //返回root元素字体计算结果
+    function _getNewFontSize() {
+        //var scale = designHeight !== 0 ? Math.min(win.innerWidth / designWidth, win.innerHeight / designHeight) : win.innerWidth / designWidth;
+        var scale = (window.innerWidth) / designWidth;
+        return parseInt(scale * 10000 * designFontSize) / 10000;
+    }
 
-          !function () {
-              rootWidth = root.getBoundingClientRect().width;
-              self = self ? self : arguments.callee;
-              //如果此时屏幕宽度不准确，就尝试再次获取分辨率，只尝试20次，否则使用win.innerWidth计算
-              if (rootWidth !== win.innerWidth && count < 20) {
-                  win.setTimeout(function () {
-                      count++;
-                      self();
-                  }, 0);
-              } else {
-                  newSize = _getNewFontSize();
-                  //如果css已经兼容当前分辨率就不管了
-                  if (newSize + 'px' !== getComputedStyle(root)['font-size']) {
-                      root.style.fontSize = newSize + "px";
-                      return callback && callback(newSize);
-                  }
-                  ;
-              }
-              ;
-          }();
-          //横竖屏切换的时候改变fontSize，根据需要选择使用
-          win.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function () {
-              clearTimeout(t);
-              t = setTimeout(function () {
-                  self();
-              }, 300);
-          }, false);
-      }(window, {
-          designWidth: 750,
-          // designHeight: 1136,
-          designFontSize: 100, //安卓下不能设置小于12px的字体 所以这个值要尽量大些
-          callback: function (argument) {
-              //console.log("test")
-          }
-      });
+    ! function() {
+        rootWidth = root.getBoundingClientRect().width;
+        self = self ? self : arguments.callee;
+        //如果此时屏幕宽度不准确，就尝试再次获取分辨率，只尝试20次，否则使用win.innerWidth计算
+        if (rootWidth !== win.innerWidth && count < 20) {
+            win.setTimeout(function() {
+                count++;
+                self();
+            }, 0);
+        } else {
+            newSize = _getNewFontSize();
+            //如果css已经兼容当前分辨率就不管了
+            if (newSize + 'px' !== getComputedStyle(root)['font-size']) {
+                root.style.fontSize = newSize + "px";
+                return callback && callback(newSize);
+            };
+        };
+    }();
+    //横竖屏切换的时候改变fontSize，根据需要选择使用
+    win.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
+        clearTimeout(t);
+        t = setTimeout(function() {
+            self();
+        }, 300);
+    }, false);
+}(window, {
+    designWidth: 750,
+    // designHeight: 1136,
+    designFontSize: 100, //安卓下不能设置小于12px的字体 所以这个值要尽量大些
+    callback: function(argument) {
+        //console.log("test")
+    }
+});
